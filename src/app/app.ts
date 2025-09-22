1e8c4a8f704caae0057280f1ab85654a86dd8cbe
José <jclaercio8@gmail.com>
2025-09-22 19:53:51 -0300

    add landingpage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('landingPage');
 }
+
---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [CommonModule],
+  templateUrl: './landing-page.html',
+  styleUrls: ['./landing-page.css']
+})
+export class LandingPage {
+slides = [
+  'bikeStreet.png',
+  'bikeGrass.png'
+  
+];
+
+currentSlide = 0;
+
+ngOnInit(): void {
+  setInterval(() => {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }, 1000);
+}
+
+          // WhatsApp function
+     openWhatsApp(): void {
+            const phoneNumber = '5584991682599'; // Número com código do país
+            const message = 'Olá! Gostaria de saber mais sobre as bicicletas da Mundial Ciclo.';
+            const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+            window.open(url, '_blank');
+        }
+
+        // Instagram function
+       openInstagram(): void {
+            // Substitua pelo @ do Instagram da loja
+            const instagramHandle = 'mundialciclo'; 
+            const url = `https://www.instagram.com/${instagramHandle}`;
+            window.open(url, '_blank');
+        }
+
+        scrollToSection(sectionId: string) {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+}
