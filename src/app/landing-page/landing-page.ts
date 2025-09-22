d8943098f89da9198f669ed223884ab861ae9bbb
José <jclaercio8@gmail.com>
2025-09-22 20:57:33 -0300

    add visual features

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -9,41 +9,42 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./landing-page.css']
 })
 export class LandingPage {
-slides = [
-  'bikeStreet.png',
-  'bikeGrass.png'
-  
-];
+  slides = [
+    'bikeStreet.png',
+    'bikeGrass.png'
 
-currentSlide = 0;
+  ];
 
-ngOnInit(): void {
-  setInterval(() => {
-    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-  }, 1000);
-}
+  currentSlide = 0;
 
-          // WhatsApp function
-     openWhatsApp(): void {
-            const phoneNumber = '5584991682599'; // Número com código do país
-            const message = 'Olá! Gostaria de saber mais sobre as bicicletas da Mundial Ciclo.';
-            const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-            window.open(url, '_blank');
-        }
-
-        // Instagram function
-       openInstagram(): void {
-            // Substitua pelo @ do Instagram da loja
-            const instagramHandle = 'mundialciclo'; 
-            const url = `https://www.instagram.com/${instagramHandle}`;
-            window.open(url, '_blank');
-        }
-
-        scrollToSection(sectionId: string) {
-  const element = document.getElementById(sectionId);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
+  ngOnInit(): void {
+    setInterval(() => {
+      console.log('Current Slide:', this.currentSlide);
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 5000);
   }
-}
+
+  // WhatsApp function
+  openWhatsApp(): void {
+    const phoneNumber = '5584991682599'; // Número com código do país
+    const message = 'Olá! Gostaria de saber mais sobre as bicicletas da Mundial Ciclo.';
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  }
+
+  // Instagram function
+  openInstagram(): void {
+    // Substitua pelo @ do Instagram da loja
+    const instagramHandle = 'mundialciclo';
+    const url = `https://www.instagram.com/${instagramHandle}`;
+    window.open(url, '_blank');
+  }
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 }
