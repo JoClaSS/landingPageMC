df53354747d38855be849240b284967907548797
José <jclaercio8@gmail.com>
2025-10-01 00:03:43 -0300

    text shadow h1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPage } from './landing-page/landing-page';
 import { Analytics } from "@vercel/analytics/next"
 import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
